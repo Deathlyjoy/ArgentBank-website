b88d63ec947b8b32a8a139808fbc4f1db9463957
Deathlyjoy <colombelle.quentin@icloud.com>
2023-10-25 08:05:53 +0200

    Add Swagger File for Transaction

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Route, Routes } from "react-router-dom";
 // PAGES
 import Home from "./pages/Home/Home";
 import SignIn from "./pages/SignIn/SignIn";
-import SignUp from "./pages/SignUp/SignUp";
 import User from "./pages/User/User";
 import Error from "./pages/Error/Error";
 
@@ -30,7 +29,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
+        
         <Route path="/user" element={<User />} />
 
         <Route path="*" element={<Error />} />
